b79c6ca4192e1402b3c63fe0c1e2892bd232c5f9
thomas <thomasrcisneroz@gmail.com>
2021-07-19 19:51:45 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+const a = 3
+const b = 4
+let num1 = 2000
+let num2 = 100
+
+
+const divide = function() {
+    return num1 / num2;
+}
+divide();
+
+const square = x => x** 2;
+
+const add = (a, b) => a + b;
+add(a, b);
